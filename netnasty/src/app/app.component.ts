7a0ffab3ec600bc1ca2de85c16f2e1a931ef0356
Matt <thepigs@gmail.com>
2018-02-05 23:14:11 +1100

    make working

--- netnasty/src/app/app.component.ts
+++ netnasty/src/app/app.component.ts
@@ -1,50 +1,35 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
-import {MediaMatcher} from '@angular/cdk/layout';
-import {ChangeDetectorRef, Component} from '@angular/core';
+	title = 'app';
+	mobileQuery: MediaQueryList;
 
-/** @title Responsive sidenav */
-@Component({
-  selector: 'sidenav-responsive-example',
-    templateUrl: 'sidenav-responsive-example.html',
-      styleUrls: ['sidenav-responsive-example.css'],
-      })
-      export class SidenavResponsiveExample {
-        mobileQuery: MediaQueryList;
+	fillerNav = Array(50).fill(0).map((_, i) => `Nav Item ${i + 1}`);
 
-	  fillerNav = Array(50).fill(0).map((_, i) => `Nav Item ${i + 1}`);
-
-	    fillerContent = Array(50).fill(0).map(() =>
-	          `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+	fillerContent = Array(50).fill(0).map(() =>
+		`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 		         labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
 			        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
 				       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
 				              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
 
-					        private _mobileQueryListener: () => void;
+	private _mobileQueryListener: () => void;
 
-						  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-						      this.mobileQuery = media.matchMedia('(max-width: 600px)');
-						          this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-							      this.mobileQuery.addListener(this._mobileQueryListener);
-							        }
+	constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+		this.mobileQuery = media.matchMedia('(max-width: 600px)');
+		this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+		this.mobileQuery.addListener(this._mobileQueryListener);
+	}
 
-								  ngOnDestroy(): void {
-								      this.mobileQuery.removeListener(this._mobileQueryListener);
-								        }
-
-									  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
-									  }
+	ngOnDestroy(): void {
+		this.mobileQuery.removeListener(this._mobileQueryListener);
+	}
+}
 
 
-									  /**  Copyright 2018 Google Inc. All Rights Reserved.
-									      Use of this source code is governed by an MIT-style license that
-									          can be found in the LICENSE file at http://angular.io/license */
--- netnasty/src/app/app.module.ts
+++ netnasty/src/app/app.module.ts
@@ -1,8 +1,84 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
+import {CdkTableModule} from '@angular/cdk/table';
+import {
+
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule
+} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ]
+})
+export class DemoMaterialModule {}
 
 
 @NgModule({
@@ -10,9 +86,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    DemoMaterialModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents:[AppComponent]
 })
 export class AppModule { }
